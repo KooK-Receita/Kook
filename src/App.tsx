6d0fff3ef611e24e867a24c0acf9af65f804ff95
victorpeigo <victorperigo@hotmail.com>
2022-04-02 18:48:43 -0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import LeftMenu from "./components/left-menu/left-menu";
 import Content from "./components/content/content";
 import { Container, Row } from "react-bootstrap";
-
 function App() {
   return (
     <Container fluid h-vh-100>
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,20 @@
-import { Card, Col, Navbar } from "react-bootstrap";
+import { Card, Col, Image, Navbar } from "react-bootstrap";
 import "./header.css";
+import pizza from "../../assets/pizza.png";
 
 function Header() {
-  return (
-    <Navbar bg="primary" variant="light">
-        bilu bilu
-    </Navbar>
-  );
+    return (
+        <Navbar className="default-bg padding-nav" variant="light">
+            <Col sm={1} className="justify-content-end">
+                <Navbar.Brand href="#" className="start-end">
+                    <Image src={pizza} />
+                </Navbar.Brand>
+            </Col>
+            <Col>
+                <h1>KOOK</h1>
+            </Col>
+        </Navbar>
+    );
 }
 
 export default Header;
