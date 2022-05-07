81ffeb9674f9d103bf70543cf02aec2dff80ba3f
caesar <joaovictorcsatu@gmail.com>
2022-05-07 18:36:54 -0300

    Update

--- src/components/adicione-sua-receita/adicione-sua-receita.tsx
+++ src/components/adicione-sua-receita/adicione-sua-receita.tsx
@@ -1,15 +1,72 @@
 import "./adicione-sua-receita.css";
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
+import ModalFiltro from "../modal/modal-filtro";
+import Modal from "react-modal";
+import { useState } from "react";
+import ModalCriar from "../modal/modal-criar";
 
 function AdicioneSuaReceita() {
+  const [modalFiltroIsOpen, setFiltroIsOpen] = useState(false);
+  const [modalCriarIsOpen, setCriarIsOpen] = useState(false);
+
+  const customStyles = {
+    content: {
+      marginRight: "15%",
+      marginLeft: "15%",
+      borderRadius: "25px",
+    },
+  };
+
   return (
     <Col md={9}>
       <div className="ads">
         <div className="inside">
-          <span>Adicione sua receita</span>
+          <span>
+            <a className="modalLink" onClick={() => setCriarIsOpen(true)}>
+              Adicione sua receita
+            </a>
+          </span>
+          <Modal isOpen={modalCriarIsOpen} style={customStyles}>
+            <ModalCriar />
+            <Button
+              variant="secondary"
+              type="button"
+              onClick={() => setCriarIsOpen(false)}
+            >
+              Voltar
+            </Button>
+            <Button
+              variant="success"
+              type="button"
+              onClick={() => setFiltroIsOpen(false)}
+            >
+              Salvar
+            </Button>
+          </Modal>
         </div>
         <div className="inside">
-          <span>Filtre uma receita</span>
+          <span>
+            <a className="modalLink" onClick={() => setFiltroIsOpen(true)}>
+              Filtre uma receita
+            </a>
+          </span>
+          <Modal isOpen={modalFiltroIsOpen} style={customStyles}>
+            <ModalFiltro />
+            <Button
+              variant="secondary"
+              type="button"
+              onClick={() => setFiltroIsOpen(false)}
+            >
+              Voltar
+            </Button>
+            <Button
+              variant="warning"
+              type="button"
+              onClick={() => setFiltroIsOpen(false)}
+            >
+              Filtrar
+            </Button>
+          </Modal>
         </div>
       </div>
     </Col>
---
+++ src/components/modal/modal-filtro.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { Col, Form, Row } from "react-bootstrap";
+
+export default function ModalFiltro() {
+  return (
+    <div>
+      <Form>
+        <Row>
+          <Col md={5}>
+            <Form.Group>
+              <Form.Label>Nome da Receita</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+          </Col>
+
+          <Col md={3}>
+            <Form.Group>
+              <Form.Label>Tempo de preparo</Form.Label>
+              <Form.Control type="number" />
+            </Form.Group>
+          </Col>
+          <Col md={2}>
+            <Form.Group>
+              <Form.Label>Unidade</Form.Label>
+              <Form.Select />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={6}>
+            <Form.Group>
+              <Form.Label>Ingrediente</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+          </Col>
+
+          <Col md={5}>
+            <Form.Group>
+              <Form.Label>Porções</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Group>
+              <Form.Label>Categoria</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+          </Col>
+        </Row>
+      </Form>
+    </div>
+  );
+}
