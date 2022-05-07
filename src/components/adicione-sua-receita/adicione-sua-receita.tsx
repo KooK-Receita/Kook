14330bf498008c3706598df461653b0f3e4177fe
Milhomen <nathanmilhomen07@hotmail.com>
2022-05-07 16:58:54 -0300

    modais

--- src/components/adicione-sua-receita/adicione-sua-receita.tsx
+++ src/components/adicione-sua-receita/adicione-sua-receita.tsx
@@ -1,15 +1,49 @@
 import "./adicione-sua-receita.css";
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
+import ModalFiltro from "../modal/modal-filtro";
+import Modal from "react-modal";
+import { useState } from "react";
+import ModalCriar from "../modal/modal-criar";
 
 function AdicioneSuaReceita() {
+  const [modalFiltroIsOpen, setFiltroIsOpen] = useState(false);
+  const [modalCriarIsOpen, setCriarIsOpen] = useState(false);
+
+  const customStyles = {
+    content: {
+      marginRight: "15%",
+      marginLeft: "15%",
+    },
+  };
+
   return (
     <Col md={9}>
       <div className="ads">
         <div className="inside">
-          <span>Adicione sua receita</span>
+          <span>
+            <a href="#" onClick={() => setCriarIsOpen(true)}>
+              Adicione sua receita
+            </a>
+          </span>
+          <Modal isOpen={modalCriarIsOpen} style={customStyles}>
+            <ModalCriar />
+            <button type="button" onClick={() => setCriarIsOpen(false)}>
+              Fechar
+            </button>
+          </Modal>
         </div>
         <div className="inside">
-          <span>Filtre uma receita</span>
+          <span>
+            <a href="javascript:void(0)" onClick={() => setFiltroIsOpen(true)}>
+              Filtre uma receita
+            </a>
+          </span>
+          <Modal isOpen={modalFiltroIsOpen} style={customStyles}>
+            <ModalFiltro />
+            <Button type="button" onClick={() => setFiltroIsOpen(false)}>
+              Fechar
+            </Button>
+          </Modal>
         </div>
       </div>
     </Col>
---
+++ src/components/modal/modal-criar.tsx
@@ -0,0 +1,77 @@
+import "./modal.css";
+import { Button, Col, Form, Row } from "react-bootstrap";
+
+export default function ModalCriar() {
+  return (
+    <div>
+      <h3>Adicione sua Receita</h3>
+      <hr />
+      <Form>
+        <Row>
+          <Col md={5}>
+            <Form.Group>
+              <Form.Label>Nome da Receita</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+          </Col>
+
+          <Col md={2}>
+            <Form.Group>
+              <Form.Label>Tempo de preparo</Form.Label>
+              <Form.Control type="number" />
+            </Form.Group>
+          </Col>
+          <Col md={2}>
+            <Form.Group>
+              <Form.Label>Unidade</Form.Label>
+              <Form.Select />
+            </Form.Group>
+          </Col>
+          <Col md={2}>
+            <Form.Group>
+              <Form.Label>Porções</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={4}>
+            <Form.Group>
+              <Form.Label>Quantidade</Form.Label>
+              <Form.Control type="number" />
+            </Form.Group>
+          </Col>
+
+          <Col md={5}>
+            <Form.Group>
+              <Form.Label>Ingrediente</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+          </Col>
+
+          <Col md={2}>
+            <Form.Group>
+              <Form.Label>Unidade</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Group>
+              <Form.Label>Categoria</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>Modo de preparo</Form.Label>
+              <textarea className="form-control"></textarea>
+            </Form.Group>
+          </Col>
+          <Col>Adicione uma imagem</Col>
+        </Row>
+      </Form>
+    </div>
+  );
+}
