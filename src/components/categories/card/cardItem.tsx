dedf31cf3ee60744315d3780baa855a2c5215651
caesar <joaovictorcsatu@gmail.com>
2022-05-09 09:44:07 -0300

    Update

--- src/components/categories/card/cardItem.tsx
+++ src/components/categories/card/cardItem.tsx
@@ -1,33 +1,35 @@
 import React, { useContext } from "react";
 import { cardContextProvider } from "./category";
 import { Card } from "react-bootstrap";
+import "./category.css";
 import data from "../../../assets/imagens_receitas/data";
 
-const CardComponent: React.FC = (item, itemIndex) => {
+const CardComponent: React.FC = () => {
   const { vetorDeElementos, index, setIndex }: any = useContext(
     cardContextProvider
   );
-  let slideClass = "nextSlide";
-
-  if (itemIndex === index) {
-    slideClass = "currentSlide";
-  }
-
-  //Can potentially break the carroussel uncomment with caution
-  if (
-    itemIndex === index - 1 ||
-    (index === 0 && itemIndex === vetorDeElementos.length - 1)
-  ) {
-    slideClass = "prevSlide";
-  }
 
   return (
-    <Card className={`${slideClass}, cards-container`}>
-      {data.map((item) => {
+    <Card className="cards-container">
+      {data.map((item, itemIndex) => {
         const { id, imagem, categoria } = item;
 
+        let slideClass = "nextSlide";
+
+        if (itemIndex === index) {
+          slideClass = "currentSlide";
+        }
+
+        //Can potentially break the carroussel uncomment with caution
+        if (
+          itemIndex === index - 1 ||
+          (index === 0 && itemIndex === vetorDeElementos.length - 1)
+        ) {
+          slideClass = "prevSlide";
+        }
+
         return (
-          <Card.Body key={id} className="category">
+          <Card.Body key={id} className={`${slideClass}, category`}>
             <img src={imagem} className="img" />
             <br />
             <Card.Text className="category-text">{categoria}</Card.Text>
