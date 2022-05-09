5650e871cea1344185f7ac689d9ae939c61ac400
caesar <joaovictorcsatu@gmail.com>
2022-05-09 19:54:51 -0300

    Update

--- src/components/categories/card/cardItem.tsx
+++ src/components/categories/card/cardItem.tsx
@@ -5,31 +5,13 @@ import "./category.css";
 import data from "../../../assets/imagens_receitas/data";
 
 const CardComponent: React.FC = () => {
-  const { vetorDeElementos, index, setIndex }: any = useContext(
-    cardContextProvider
-  );
-
   return (
     <Card className="cards-container">
-      {data.map((item, itemIndex) => {
+      {data.map((item) => {
         const { id, imagem, categoria } = item;
 
-        let slideClass = "nextSlide";
-
-        if (itemIndex === index) {
-          slideClass = "currentSlide";
-        }
-
-        //Can potentially break the carroussel uncomment with caution
-        if (
-          itemIndex === index - 1 ||
-          (index === 0 && itemIndex === vetorDeElementos.length - 1)
-        ) {
-          slideClass = "prevSlide";
-        }
-
         return (
-          <Card.Body key={id} className={`${slideClass}, category`}>
+          <Card.Body key={id} className="category">
             <img src={imagem} className="img" />
             <br />
             <Card.Text className="category-text">{categoria}</Card.Text>
